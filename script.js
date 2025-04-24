75c1da03af5e66313d61d84a8ff8f0d7bfe4d52c
naimjalal <naimjalal@gmail.com>
2025-04-24 23:59:32 +0300

    each cells clicked in browser

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // console.log('Welcome')
+let player = 'x'
 const board = document.querySelector('.board')
+
 for (let i = 1; i < 42; i++) {
   const cell = document.createElement('div')
   cell.classList.add('cell')
   board.appendChild(cell)
+}
+
+const cells = document.querySelectorAll('.cell')
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', () => {
+    if (cells[i].innerText === '') {
+      cells[i].style.backgroundColor = 'red'
+    }
+  })
 }
