518a916770b2de4ee0e44917dff62847ca4852bd
naimjalal <naimjalal@gmail.com>
2025-04-26 14:32:24 +0300

    done logic of bottom to top cells

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const board = document.querySelector('.board')
+const message = document.getElementById('message')
+let currentPlayer = 'red'
 
 for (let i = 1; i < 42; i++) {
   const cell = document.createElement('div')
@@ -6,22 +8,40 @@ for (let i = 1; i < 42; i++) {
   board.appendChild(cell)
 }
 
-let currentPlayer = 'red'
 const cells = document.querySelectorAll('.cell')
-const message = document.getElementById('message')
+
+const coloumnPatterns = [
+  [35, 28, 21, 14, 7, 0],
+  [36, 29, 22, 15, 8, 1],
+  [37, 30, 23, 16, 9, 2],
+  [38, 31, 24, 17, 10, 3],
+  [39, 32, 25, 18, 11, 4],
+  [40, 33, 26, 19, 12, 5],
+  [41, 34, 27, 20, 13, 6]
+]
 
 for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener('click', () => {
-    // console.log(' clicked cell index:', i)
-    if (cells[i].style.backgroundColor === '') {
-      if (currentPlayer === 'red') {
-        cells[i].style.backgroundColor = 'red'
-        currentPlayer = 'yellow'
-        message.innerText = "Player Yellow's turn"
-      } else {
-        cells[i].style.backgroundColor = 'yellow'
-        currentPlayer = 'red'
-        message.innerText = "Player Red's turn"
+    //console.log(' clicked cell index:', i)
+    const column = i % 7
+    const columnIndexes = coloumnPatterns[column]
+
+    for (let j = 0; j < columnIndexes.length; j++) {
+      const index = columnIndexes[j]
+
+      if (cells[index].style.backgroundColor === '') {
+        if (currentPlayer === 'red') {
+          cells[index].style.backgroundColor = 'red'
+          currentPlayer = 'yellow'
+          message.innerText = "Player Yellow's turn"
+          message.style.color = 'yellow'
+        } else {
+          cells[index].style.backgroundColor = 'yellow'
+          currentPlayer = 'red'
+          message.innerText = "Player Red's turn"
+          message.style.color = 'red'
+        }
+        break
       }
     }
   })
